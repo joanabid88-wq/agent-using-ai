9b2281f972333278eb0892a7430234a96d372767
joanabid-streamlit-first-project <joanabid88@gmail.com>
2025-11-03 21:21:20 +0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -180,11 +180,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-st.sidebar.subheader("🧩 Data Sources")
-use_wiki = st.sidebar.checkbox("Wikipedia", True)
-use_duck = st.sidebar.checkbox("DuckDuckGo", True)
-use_youtube = st.sidebar.checkbox("YouTube Search", False)
-use_arxiv = st.sidebar.checkbox("Arxiv", False)
+
 
 
 
